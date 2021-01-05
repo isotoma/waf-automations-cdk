a2373ea0419a28b0df4d20b0ddb528db54429b1b
Andrew Plummer <plummer574@gmail.com>
2021-01-05 16:28:08 +0000

    Fix for updates and handle selected template version

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # waf-automations-cdk
 
+## Usage
+
+```typescript
+new WafSecurityAutomations(this, 'waf-security-automations', {
+    stackName: 'waf-security-automations',
+    accessLogBucket: myLogBucket,
+    options: {
+        // See below
+    },
+});
+```
+
+### Options
+
+All are optional
+
+| Attribute         | Default    | Description                                                                                                                                       |
+| ---------         | -------    | -----------                                                                                                                                       |
+| `templateVersion` | `'latest'` | See [releases](https://github.com/awslabs/aws-waf-security-automations/releases). Best to select a version rather than using the dynamic `latest` |
+
 ## Development
 
 ### Releasing a new version
--- provider/index.ts
+++ provider/index.ts
@@ -7,67 +7,83 @@ import {
 } from 'aws-lambda';
 import * as AWS from 'aws-sdk';
 
-const TemplateURL = 'https://s3.amazonaws.com/solutions-reference/aws-waf-security-automations/latest/aws-waf-security-automations.template';
+const log = (item: Record<string, any>): void => {
+    console.log(JSON.stringify(item));
+};
+
+const getTemplateUrl = (version: string): string => {
+    return `https://s3.amazonaws.com/solutions-reference/aws-waf-security-automations/${version}/aws-waf-security-automations.template`;
+};
+
+const sdkArgsForCreateOrUpdate = (event: CloudFormationCustomResourceCreateEvent | CloudFormationCustomResourceUpdateEvent): AWS.CloudFormation.Types.CreateStackInput & AWS.CloudFormation.Types.UpdateStackInput => {
+    const accessLogBucketName = event.ResourceProperties.AccessLogBucketName;
+    const stackName = event.ResourceProperties.StackName;
+    const templateUrl = getTemplateUrl(event.ResourceProperties.TemplateVersion ?? 'latest');
+
+    return {
+        StackName: stackName,
+        ClientRequestToken: event.RequestId,
+        TemplateURL: templateUrl,
+        Capabilities: ['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM', 'CAPABILITY_AUTO_EXPAND'],
+        Parameters: [
+            {
+                ParameterKey: 'AppAccessLogBucket',
+                ParameterValue: accessLogBucketName,
+            },
+        ],
+    };
+};
 
 export const onCreate = async (event: CloudFormationCustomResourceCreateEvent): Promise<any> => {
-    const accessLogBucketName = event.ResourceProperties.AccessLogBucketName;
-    const StackName = event.ResourceProperties.StackName;
+    log({message: 'Running onCreate'});
+    const args = sdkArgsForCreateOrUpdate(event);
     const cfn = new AWS.CloudFormation({ region: 'us-east-1' });
-    try {
-        const response = await cfn
-            .createStack({
-                StackName,
-                ClientRequestToken: event.RequestId,
-                TemplateURL,
-                Capabilities: ['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM', 'CAPABILITY_AUTO_EXPAND'],
-                Parameters: [
-                    {
-                        ParameterKey: 'AppAccessLogBucket',
-                        ParameterValue: accessLogBucketName,
-                    },
-                ],
-            })
-            .promise();
-        return {
-            PhysicalResourceId: StackName,
-        };
-    } catch (err) {
-        return {
-            Status: 'FAILED',
-            Reason: `${err}`,
-            PhysicalResourceId: StackName,
-        };
-    }
+    const response = await cfn
+        .createStack(args)
+        .promise();
+    log({message: 'Ran createStack', response});
+    return {
+        PhysicalResourceId: args.StackName,
+    };
+};
+
+export const onUpdate = async (event: CloudFormationCustomResourceUpdateEvent): Promise<any> => {
+    log({message: 'Running onUpdate'});
+    const args = sdkArgsForCreateOrUpdate(event);
+    const cfn = new AWS.CloudFormation({ region: 'us-east-1' });
+    const response = await cfn
+        .updateStack(args)
+        .promise();
+    log({message: 'Ran updateStack', response});
+    return {
+        PhysicalResourceId: args.StackName,
+    };
 };
 
 export const onDelete = async (event: CloudFormationCustomResourceDeleteEvent): Promise<any> => {
+    log({message: 'Running onDelete'});
+
     const cfn = new AWS.CloudFormation({ region: 'us-east-1' });
-    try {
-        const response = await cfn
-            .deleteStack({
-                StackName: event.PhysicalResourceId,
-                ClientRequestToken: event.RequestId,
-            })
-            .promise();
-        return {
-            PhysicalResourceId: event.PhysicalResourceId,
-        };
-    } catch (err) {
-        return {
-            Status: 'FAILED',
-            Reason: `${err}`,
-            PhysicalResourceId: event.PhysicalResourceId,
-        };
-    }
+    const response = await cfn
+        .deleteStack({
+            StackName: event.PhysicalResourceId,
+            ClientRequestToken: event.RequestId,
+        })
+        .promise();
+    log({message: 'Ran deleteStack', response});
+    return {
+        PhysicalResourceId: event.PhysicalResourceId,
+    };
 };
 
 export const onEvent = (event: CloudFormationCustomResourceEvent): Promise<CloudFormationCustomResourceResponse> => {
-    console.log(JSON.stringify(event));
+    log({message: 'Starting onEvent', event})
     try {
         switch (event.RequestType) {
             case 'Create':
-            case 'Update':
                 return onCreate(event as CloudFormationCustomResourceCreateEvent);
+            case 'Update':
+                return onUpdate(event as CloudFormationCustomResourceUpdateEvent);
             case 'Delete':
                 return onDelete(event as CloudFormationCustomResourceDeleteEvent);
             default:
@@ -80,7 +96,7 @@ export const onEvent = (event: CloudFormationCustomResourceEvent): Promise<Cloud
 };
 
 export const isComplete = async (event: CloudFormationCustomResourceEvent): Promise<any> => {
-    console.log(JSON.stringify(event));
+    log({message: 'Starting isComplete', event})
     const StackName = event.ResourceProperties.StackName;
     const cfn = new AWS.CloudFormation({ region: 'us-east-1' });
     const response = await cfn
